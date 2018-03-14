791c46768b44b2a8b6af516f228cd989bcf87efc
Stanislav Komanec <stanislav.komanec@kiwi.com>
2018-03-14 17:23:08 +0100

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 0.1.1 (2018-03-14)
+
+Make imports more concise
+
 ## 0.1.0 (2018-01-15)
 
 Initial release.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('test-requirements.in') as f:
 
 setup(
     name='kiwi-structlog-config',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/kiwicom/kiwi-structlog-config',
     author='Platform Team',
     author_email='platform@kiwi.com',
